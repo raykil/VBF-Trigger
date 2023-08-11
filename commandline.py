f82f9a3bc26163eab0878369cc52ae0a2083266a
Raymond Kil <jungmink@bu.edu>
2023-08-11 13:14:40 +0900

    plots made, Aug 11 update

--- commandline.py
+++ commandline.py
@@ -6,17 +6,17 @@ commands = [
     'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="MjjAnalysis" --outputdir="./jetlevel_effplots/" --shape=True',
     'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="DetaAnalysis" --outputdir="./jetlevel_effplots/" --shape=True',
 
-    'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="LeadJetchEmEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True',
-    'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="LeadJetchHEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True',
-    'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="LeadJetneEmEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True',
-    'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="LeadJetneHEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True',
-    'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="LeadJetmuEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True',
+    #'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="LeadJetchEmEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True',
+    #'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="LeadJetchHEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True',
+    #'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="LeadJetneEmEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True',
+    #'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="LeadJetneHEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True',
+    #'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="LeadJetmuEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True',
 
-    'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="SubLeadJetchEmEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True',
-    'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="SubLeadJetchHEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True',
-    'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="SubLeadJetneEmEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True',
-    'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="SubLeadJetneHEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True',
-    'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="SubLeadJetmuEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True',
+    #'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="SubLeadJetchEmEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True',
+    #'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="SubLeadJetchHEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True',
+    #'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="SubLeadJetneEmEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True',
+    #'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="SubLeadJetneHEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True',
+    #'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="SubLeadJetmuEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True',
 
     # Tight cuts
     'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="LeadJetPtAnalysis" --outputdir="./jetlevel_effplots/" --shape=True  --tightcuts=True',
@@ -24,17 +24,17 @@ commands = [
     'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="MjjAnalysis" --outputdir="./jetlevel_effplots/" --shape=True --tightcuts=True',
     'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="DetaAnalysis" --outputdir="./jetlevel_effplots/" --shape=True --tightcuts=True',
 
-    'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="LeadJetchEmEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True --tightcuts=True',
-    'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="LeadJetchHEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True --tightcuts=True',
-    'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="LeadJetneEmEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True --tightcuts=True',
-    'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="LeadJetneHEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True --tightcuts=True',
-    'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="LeadJetmuEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True --tightcuts=True',
+    #'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="LeadJetchEmEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True --tightcuts=True',
+    #'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="LeadJetchHEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True --tightcuts=True',
+    #'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="LeadJetneEmEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True --tightcuts=True',
+    #'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="LeadJetneHEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True --tightcuts=True',
+    #'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="LeadJetmuEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True --tightcuts=True',
 
-    'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="SubLeadJetchEmEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True --tightcuts=True',
-    'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="SubLeadJetchHEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True --tightcuts=True',
-    'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="SubLeadJetneEmEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True --tightcuts=True',
-    'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="SubLeadJetneHEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True --tightcuts=True',
-    'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="SubLeadJetmuEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True --tightcuts=True'
+    #'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="SubLeadJetchEmEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True --tightcuts=True',
+    #'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="SubLeadJetchHEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True --tightcuts=True',
+    #'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="SubLeadJetneEmEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True --tightcuts=True',
+    #'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="SubLeadJetneHEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True --tightcuts=True',
+    #'python efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="SubLeadJetmuEFAnalysis" --outputdir="./jetlevel_effplots/" --shape=True --tightcuts=True'
 ]
 
 counter = 1
