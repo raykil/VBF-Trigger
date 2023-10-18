116ba144d8c97e5884317275fe63f6e527020467
Raymond Kil <jungmink@bu.edu>
2023-10-18 03:40:25 +0200

    cleanup1

--- efficiency_analysis/efficiency_submit/efficiencyanalysis3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate vbftrigger
-
-python /eos/user/j/jkil/SUEP/vbftrigger/efficiency_analysis/efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="LeadJetPtAnalysis" --outputdir="/eos/user/j/jkil/www/VBFSUEP/efficiency/withJetLevelHLT/" --shape=True --tightcuts=True --datapath=/eos/user/j/jkil/SUEP/vbftrigger/efficiency/2023CD_parquets/ &> /afs/cern.ch/user/j/jkil/efficiencyanalysis/batchlogs/LeadJetPtAnalysis_tight.log
-python /eos/user/j/jkil/SUEP/vbftrigger/efficiency_analysis/efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="SubleadJetPtAnalysis" --outputdir="/eos/user/j/jkil/www/VBFSUEP/efficiency/withJetLevelHLT/" --shape=True --tightcuts=True --datapath=/eos/user/j/jkil/SUEP/vbftrigger/efficiency/2023CD_parquets/ &> /afs/cern.ch/user/j/jkil/efficiencyanalysis/batchlogs/SubleadJetPtAnalysis_tight.log
-python /eos/user/j/jkil/SUEP/vbftrigger/efficiency_analysis/efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="MjjAnalysis" --outputdir="/eos/user/j/jkil/www/VBFSUEP/efficiency/withJetLevelHLT/" --shape=True --tightcuts=True --datapath=/eos/user/j/jkil/SUEP/vbftrigger/efficiency/2023CD_parquets/ &> /afs/cern.ch/user/j/jkil/efficiencyanalysis/batchlogs/MjjAnalysis_tight.log
-python /eos/user/j/jkil/SUEP/vbftrigger/efficiency_analysis/efficiencyanalysis.py --whichfiles="singlemuon" --triggerpath="pt105Analysis" --analysis="DetaAnalysis" --outputdir="/eos/user/j/jkil/www/VBFSUEP/efficiency/withJetLevelHLT/" --shape=True --tightcuts=True --datapath=/eos/user/j/jkil/SUEP/vbftrigger/efficiency/2023CD_parquets/ &> /afs/cern.ch/user/j/jkil/efficiencyanalysis/batchlogs/DetaAnalysis_tight.log
